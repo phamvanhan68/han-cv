fddd76a6c3af1a2e8fddbb1c744e259dee9a4199
Han (Hanson) V. PHAM <hapham@philly.com>
2020-12-20 20:58:29 +0700

    Updates

---
+++ precache-manifest.53385b3b91e166c8a8bc39ae75911247.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "375625956fc44b72b5f4256985c65e44",
+    "url": "/han-cv/index.html"
+  },
+  {
+    "revision": "5beeca209e914430e42e",
+    "url": "/han-cv/static/css/2.220ccb42.chunk.css"
+  },
+  {
+    "revision": "e0c102ee97054de7ab8a",
+    "url": "/han-cv/static/css/main.c37751de.chunk.css"
+  },
+  {
+    "revision": "5beeca209e914430e42e",
+    "url": "/han-cv/static/js/2.d1df7829.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/han-cv/static/js/2.d1df7829.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e0c102ee97054de7ab8a",
+    "url": "/han-cv/static/js/main.48fe6dbd.chunk.js"
+  },
+  {
+    "revision": "fcd6ca308b0815a445d6",
+    "url": "/han-cv/static/js/runtime-main.4d773455.js"
+  },
+  {
+    "revision": "50d5dc50fbe2dfb8a99cd1e117bb3295",
+    "url": "/han-cv/static/media/duytan_logo.50d5dc50.png"
+  },
+  {
+    "revision": "ef82b5abf9dbf080dbb0acd63020eca9",
+    "url": "/han-cv/static/media/framgia_logo.ef82b5ab.png"
+  },
+  {
+    "revision": "994977c48fde58ac674a2d05ba5a5efb",
+    "url": "/han-cv/static/media/nodejs_logo_large.994977c4.png"
+  },
+  {
+    "revision": "a72ba292cc2d122960a41c6c99eb365f",
+    "url": "/han-cv/static/media/python_logo_large.a72ba292.jpg"
+  },
+  {
+    "revision": "8c0e654e03d16f28a6b0536877b4a515",
+    "url": "/han-cv/static/media/reactjs_logo_large.8c0e654e.png"
+  }
+]);
